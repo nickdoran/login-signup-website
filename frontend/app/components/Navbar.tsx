57ab6eb34abb71ada6f5777c5377ab33a73076f4
Nicklas Doran <nickdoran6@gmail.com>
2025-11-13 18:14:58 -0500

    Added/Fixed: navbar styles and added signup button

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import Login from "./buttons/Login";
+import Signup from "./buttons/Signup";
 const Navbar = () => {
     return (
         <div
-            className="w-full h-15 bg-[#181818] px-20 py-2.5 flex items-center justify-between  
+            className="w-full h-15 bg-[#282828] px-40 py-2.5 flex items-center justify-between  
     "
         >
             <p className="text-white font-bold text-2xl">Navbar</p>
-            <div>
+            <div
+                className="flex gap-3
+            "
+            >
                 <Login></Login>
+                <Signup></Signup>
             </div>
         </div>
     );
