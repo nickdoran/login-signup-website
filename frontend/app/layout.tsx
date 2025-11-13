a6f3b7bd42f662d9ea1433318ab189d521bfc089
Nicklas Doran <nickdoran6@gmail.com>
2025-11-13 18:26:51 -0500

    Added: Footer, and fixed body tag and added a div wrapper

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,12 +1,18 @@
 import "./globals.css";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 export default function RootLayout({
     children,
 }: Readonly<{ children: React.ReactNode }>) {
     return (
         <html lang="en">
-            <Navbar></Navbar>
-            <body className="bg-[#101010]">{children}</body>
+            <body className="bg-[#0a0a0a]">
+                <div className="flex flex-col h-screen">
+                    <Navbar></Navbar>
+                    {children}
+                    <Footer></Footer>
+                </div>
+            </body>
         </html>
     );
 }
