aec5006b0bf98c896300f625ab56773940824199
Nicklas Doran <nickdoran6@gmail.com>
2025-11-13 18:25:14 -0500

    Added Welcome Message comp

---
+++ frontend/app/WelcomeMessage.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const WelcomeMessage = () => {
+    return (
+        <div className="text-7xl font-bold">
+            <p>Hello, Guest</p>
+        </div>
+    );
+};
+
+export default WelcomeMessage;
