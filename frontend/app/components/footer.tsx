0fe9ecfb8ffe81858b5effe25dea89575ab4161a
Nicklas Doran <nickdoran6@gmail.com>
2025-11-13 17:50:12 -0500

    Comp: added footer comp

---
+++ frontend/app/components/footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const footer = () => {
+    return (
+        <div className="w-full h-7 bg-slate-400">
+            <p className="bg-slate-100">This is a footer</p>
+        </div>
+    );
+};
+
+export default footer;
