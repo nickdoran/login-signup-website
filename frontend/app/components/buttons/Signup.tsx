a137537001b2504304ba55f6d6c1cb1d9e9a38c1
Nicklas Doran <nickdoran6@gmail.com>
2025-11-13 18:11:16 -0500

    Added: signup Comp

---
+++ frontend/app/components/buttons/Signup.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+const Signup = () => {
+    return (
+        <Link
+            href={""}
+            className="px-2 py-1 text-[#f2f2f2] bg-[#a9a9a9] rounded-xl text-lg font-semibold"
+        >
+            Sign Up
+        </Link>
+    );
+};
+
+export default Signup;
