71870746550510057cc546165ede272cd9704baa
Nicklas Doran <nickdoran6@gmail.com>
2025-11-13 18:24:45 -0500

    Style update

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Signup from "./buttons/Signup";
 const Navbar = () => {
     return (
         <div
-            className="w-full h-15 bg-[#282828] px-40 py-2.5 flex items-center justify-between  
+            className="w-full h-17 bg-[#1d1d1d] px-40 py-2.5 flex items-center justify-between  
     "
         >
             <p className="text-white font-bold text-2xl">Navbar</p>
