e34f56d67e3bdf6e740de728511e0a6765d2793e
Nicklas Doran <nickdoran6@gmail.com>
2025-11-13 17:55:39 -0500

    File: added login button

---
+++ frontend/app/components/buttons/login.tsx
@@ -0,0 +1,15 @@
+"use client";
+import Link from "next/link";
+
+const login = () => {
+    return (
+        <Link
+            href={""}
+            className="px-2 py-1 bg-slate-600 rounded-xl font-semibold"
+        >
+            Log In
+        </Link>
+    );
+};
+
+export default login;
