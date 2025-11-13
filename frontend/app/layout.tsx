3a53e4a688cf96ee45554777d9ff377585ff75df
Nicklas Doran <nickdoran6@gmail.com>
2025-11-13 18:09:07 -0500

    Added: navbar for testing in layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,9 +1,12 @@
+import "./globals.css";
+import Navbar from "./components/Navbar";
 export default function RootLayout({
     children,
 }: Readonly<{ children: React.ReactNode }>) {
     return (
         <html lang="en">
-            <body>{children}</body>
+            <Navbar></Navbar>
+            <body className="bg-[#101010]">{children}</body>
         </html>
     );
 }
