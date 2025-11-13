fda2014223fe5b60516d8ac0ee5decb71efb35ce
Nicklas Doran <nickdoran6@gmail.com>
2025-11-13 18:14:36 -0500

    Fixed: signup button styles

--- frontend/app/components/buttons/Signup.tsx
+++ frontend/app/components/buttons/Signup.tsx
@@ -4,7 +4,7 @@ const Signup = () => {
     return (
         <Link
             href={""}
-            className="px-2 py-1 text-[#f2f2f2] bg-[#a9a9a9] rounded-xl text-lg font-semibold"
+            className="px-2 py-1 text-[#1a1a1a] bg-[#dfdfdf] rounded-xl text-lg font-semibold"
         >
             Sign Up
         </Link>
