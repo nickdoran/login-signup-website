e6b3ad83a2c85aecff724fae293e667947fdc9eb
Nicklas Doran <nickdoran6@gmail.com>
2025-11-14 18:42:53 -0500

    Added: link for signup button in home

--- frontend/app/components/buttons/Signup.tsx
+++ frontend/app/components/buttons/Signup.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Signup = () => {
     return (
         <Link
-            href={""}
+            href={"/signup"}
             className="px-2 py-1 text-[#1a1a1a] bg-[#dfdfdf] rounded-xl text-md font-semibold"
         >
             Sign Up
