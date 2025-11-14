de2004b512c1a1fd870c0d8f5e07c109ce039837
Nicklas Doran <nickdoran6@gmail.com>
2025-11-14 18:43:06 -0500

    Added: login link for login button in home

--- frontend/app/components/buttons/login.tsx
+++ frontend/app/components/buttons/login.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Login = () => {
     return (
         <Link
-            href={""}
+            href={"/login"}
             className="px-2 py-1 text-[#939393] rounded-xl text-md font-semibold"
         >
             Log In
