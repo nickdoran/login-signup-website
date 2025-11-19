4df7f564f42bd050b56ec7010559e669935e3ff8
Nicklas Doran <nickdoran6@gmail.com>
2025-11-18 22:14:49 -0500

    Added: credentials for functions that call the endpoints

--- frontend/app/services/authServices.ts
+++ frontend/app/services/authServices.ts
@@ -4,6 +4,7 @@ export const login = async (email: string, password: string) => {
     try {
         const response = await fetch(`${API_URL}/api/auth/login`, {
             method: "POST",
+            credentials: "include",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ email, password }),
         });
@@ -26,6 +27,7 @@ export const signup = async (name: string, email: string, password: string) => {
     try {
         const response = await fetch(`${API_URL}/api/auth/signup`, {
             method: "POST",
+            credentials: "include",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ name, email, password }),
         });
@@ -40,6 +42,51 @@ export const signup = async (name: string, email: string, password: string) => {
         return response.json();
     } catch (error) {
         console.error("Signup error:", error);
+        throw error;
+    }
+};
+
+export const getMe = async () => {
+    try {
+        const response = await fetch(`${API_URL}/api/auth/me`, {
+            method: "GET",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            cache: "no-store",
+        });
+        if (!response.ok) {
+            const error = await response
+                .json()
+                .catch(() => ({ detail: "Could not fetch users data" }));
+            throw new Error(error.detail || "Could not fetch users data");
+        }
+
+        const data = await response.json();
+
+        return data;
+    } catch (error) {
+        console.log("Could not fetch users name", error);
+        throw error;
+    }
+};
+
+export const logout = async () => {
+    try {
+        const response = await fetch(`${API_URL}/api/auth/logout`, {
+            method: "POST",
+            credentials: "include",
+            headers: { "Content-Type": "application/json" },
+        });
+
+        if (!response.ok) {
+            throw new Error("Logout failed");
+        }
+
+        return response.json();
+    } catch (error) {
+        console.error("Logout error:", error);
         throw error;
     }
 };
