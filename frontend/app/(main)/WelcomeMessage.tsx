1d1e98ace084610becab022211ca1e023d156ade
Nicklas Doran <nickdoran6@gmail.com>
2025-11-18 22:20:51 -0500

    Added Welcome message component.

--- frontend/app/(main)/WelcomeMessage.tsx
+++ frontend/app/(main)/WelcomeMessage.tsx
@@ -1,7 +1,36 @@
 "use client";
 
+import { useEffect, useState } from "react";
+import { getMe } from "../services/authServices";
+import { User } from "../types/user";
+
 const WelcomeMessage = () => {
-    return <p className="text-7xl font-bold fadeInMoveUp ">Hello, Guest</p>;
+    const [user, setUser] = useState<User | null>(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const getUserData = async () => {
+            try {
+                const data = await getMe();
+                setUser(data);
+            } catch (error) {
+                console.error("Could not fetch user's data", error);
+            } finally {
+                setLoading(false);
+            }
+        };
+        getUserData();
+    }, []);
+
+    if (loading) {
+        return <p className="text-7xl font-bold fadeInMoveUp">Loading...</p>;
+    }
+
+    return (
+        <p className="text-7xl font-bold fadeInMoveUp">
+            Hello, {user?.name || "Guest"}
+        </p>
+    );
 };
 
 export default WelcomeMessage;
